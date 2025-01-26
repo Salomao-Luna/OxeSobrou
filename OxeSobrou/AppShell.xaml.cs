f9306e7a734548861febd82815bbc45e8ea61438
salom <salom@LAPTOP-BONU5O0I>
2025-01-26 16:11:30 -0300

    Add project files.

---
+++ OxeSobrou/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OxeSobrou
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OxeSobrou/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OxeSobrou
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
